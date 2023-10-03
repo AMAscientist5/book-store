8e6517a57a0a59d7bf1a3a2e053745719337cafd
ahmad muas <amaahmadmusa@gmail.com>
2023-10-03 12:12:01 +0600

    some update

--- src/shared/mainApi.tsx
+++ src/shared/mainApi.tsx
@@ -1,2 +1,4 @@
-const baseUrl = "http://localhost:5000/api/v1";
+// const baseUrl = "http://localhost:5000/api/v1";
+const baseUrl =
+  "https://book-store-backend-pz1b1zh49-amascientist5.vercel.app/api/v1";
 export const env = { baseUrl };
