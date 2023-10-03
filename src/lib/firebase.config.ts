3977e5f8fd860a0376bb2d32b468101e58916036
ahmad muas <amaahmadmusa@gmail.com>
2023-10-03 10:40:08 +0600

    second commit

---
+++ src/lib/firebase.config.ts
@@ -0,0 +1,18 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDG67PYjdcOvuls2qX6zjmS23ktW3w7Ndw",
+//   authDomain: "bookstore-55a5a.firebaseapp.com",
+//   projectId: "bookstore-55a5a",
+//   storageBucket: "bookstore-55a5a.appspot.com",
+//   messagingSenderId: "321013263468",
+//   appId: "1:321013263468:web:badca78665a1b7273afc49",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+
+// //project-321013263468
